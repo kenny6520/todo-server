d82ff987133598aac72326a9d4756b6918849a3f
kenny6520 <lishiwen@wclound.cn>
2021-02-14 09:22:49 +0800

    config🔧 update docker-compose

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,18 +1,50 @@
+import * as request from "supertest";
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoModule } from './todo.module';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
-  let controller: TodoController;
+  // let todoController: TodoController;
+  // let todoService: TodoService;
+
+  let app: INestApplication;
+  let todoService = {
+    findAll(){
+      return []
+    }
+  };
 
   beforeEach(async () => {
+    // const module: TestingModule = await Test.createTestingModule({
+    //   imports: [TodoModule],
+    //   controllers: [TodoController],
+    //   providers: [TodoService],
+    // }).compile();
+
+    // todoController = module.get<TodoController>(TodoController);
+    // todoService = module.get<TodoService>(TodoService);
+
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [TodoController],
-    }).compile();
+      imports: [TodoModule]
+    })
+    .overrideProvider(TodoService)
+    .useValue(todoService)
+    .compile();
 
-    controller = module.get<TodoController>(TodoController);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('findAll', () => {
+    it("GET todo, should return an array", () => {
+      return request(app.getHttpServer())
+        .get('todo')
+        .expect(200)
+        .expect({
+          data: todoService.findAll()
+        })
+    })
+  })
 });
